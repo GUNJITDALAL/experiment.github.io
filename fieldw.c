73bd254af63b589200d4f5e7f421917e0306b254
gunjit <73329409+GUNJITDALAL@users.noreply.github.com>
2021-01-23 22:42:22 +0530

    Add files via upload

---
+++ fieldw.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int a=10;
+    float value=10.2456;
+    printf("%d",a);
+    printf("\n%5d",a);
+    printf("\n%20.3f",value);
+    return 0;
+}
